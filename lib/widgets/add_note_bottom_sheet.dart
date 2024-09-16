f28c2f4991684ab1c7f24da15b78605c8b80f336
Your Name <e.oracle2000@gmail.com>
2024-09-16 14:03:22 +0300

    create nots state

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:my_first_note_app/cubits/add_notes_cubit/cubit/add_notes_cubit.dart';
 import 'package:my_first_note_app/widgets/add_note_form.dart';
 
@@ -28,9 +27,16 @@ class _AddNoteBottomSheetState extends State<AddNoteBottomSheet> {
             }
           },
           builder: (context, state) {
-            return ModalProgressHUD(
-                inAsyncCall: state is AddNotesLoading ? true : false,
-                child: SingleChildScrollView(child: AddNoteForm()));
+            return AbsorbPointer(
+              absorbing: state is AddNotesLoading ? true : false,
+              child: Padding(
+                padding: EdgeInsets.only(
+                    left: 16,
+                    right: 16,
+                    bottom: MediaQuery.of(context).viewInsets.bottom),
+                child: SingleChildScrollView(child: AddNoteForm()),
+              ),
+            );
           },
         ),
       ),
